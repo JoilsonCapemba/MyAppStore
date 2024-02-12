39e9d91efcf512a6b2e367d01505c974c2673434
JoilsonCapemba <joilsoncapemba@gmail.com>
2024-02-12 05:13:46 +0100

    Criacao da factory e seed do produto

--- database/migrations/2024_02_12_020021_create_products_table.php
+++ database/migrations/2024_02_12_020021_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('cover')->nullable();
+            $table->decimal('price')->default('0.00');
+            $table->text('description')->nullable();
+            $table->integer('stock')->default(0);
             $table->timestamps();
         });
     }
